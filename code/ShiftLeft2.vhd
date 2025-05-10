489340eab69218d324fe2d493fe209cc1b311dbe
YassinMohamed10 <168344567+YassinMohamed10@users.noreply.github.com>
2025-05-10 18:06:49 +0300

    yassin abo issa

---
+++ code/ShiftLeft2.vhd
@@ -0,0 +1,16 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity ShiftLeft2 is
+  port( 
+        input  : in std_logic_vector(25 downto 0);
+        output : out std_logic_vector(27 downto 0) );
+end ShiftLeft2;
+
+architecture Behavioral of ShiftLeft2 is
+begin
+
+  output <= input & "00";
+
+end Behavioral;
